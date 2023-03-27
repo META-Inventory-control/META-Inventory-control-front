eedfcda8fdb906924a92cd540b84418f803dde5c
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-03-27 14:19:12 -0300

    fix: navigate to dashboard even when login not occur / Felipe

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,13 @@
 import LoginForm from "../components/loginForm"
+import { useEffect } from "react"
 
 
 const LoginPage = () => {
+
+    useEffect(() => {
+        localStorage.clear()
+    }, [])
+
     return (
         <>
             <LoginForm></LoginForm>
